629bf6cab26da295f590913547c86e41a61009bd
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2023-03-10 13:51:18 -0300

    Botando abas no streamlit. Task 0

--- meus-scripts/python-anywhere/app.py
+++ meus-scripts/python-anywhere/app.py
@@ -1,10 +1,23 @@
 # https://www.alura.com.br/artigos/ambientes-virtuais-em-python
 
+from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
 import yfinance as yf
 
-st.header('Dashboard Margem Líquida')
-st.text("Gráfico PETZ.SA")
+st.set_page_config(
+    page_title="Dashboard Margem Líquida",
+    page_icon="chart_with_upwards_trend",
+    layout="wide",
+)
 
-df = yf.download("PETZ3.SA")
-st.line_chart(df)
+with st.sidebar:
+    tabs = on_hover_tabs(tabName=['Tesouro Direto', 'Relatório Focus'], 
+                         iconName=['dashboard', 'money', ], default_choice=0)
+
+if tabs =='Tesouro Direto':
+    st.title("Tesouro direto")
+    st.write('Name of option is {}'.format(tabs))
+
+elif tabs == 'Relatório Focus':
+    st.title("Relatório Focus")
+    st.write('Name of option is {}'.format(tabs))
